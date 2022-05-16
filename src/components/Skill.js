963d5293be3db0932d83e6c5acc991a3e47687dd
Shelley Bassett <m.bassett@y7mail.com>
2022-05-16 12:24:47 +1000

    Updated content

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Monitor, Users, Zap, Search, Activity, Award, GitHub, Database, Cpu, HelpCircle } from "react-feather";
+import { Monitor, Users, Zap, Search, Activity, Award, GitHub, Database, Cpu, HelpCircle, MapPin, Code } from "react-feather";
 
 export default function Skill(props) {
-    const iconSvg = {'monitor': <Monitor />, 'users': <Users />, 'zap': <Zap />, 'search': <Search />, 'activity': <Activity />, 'award': <Award />, 'github': <GitHub />, 'database': <Database />, 'cpu': <Cpu />, 'help': <HelpCircle />,};
+    const iconSvg = {'monitor': <Monitor />, 'users': <Users />, 'zap': <Zap />, 'search': <Search />, 'activity': <Activity />, 'award': <Award />, 'github': <GitHub />, 'database': <Database />, 'cpu': <Cpu />, 'help': <HelpCircle />, 'map': <MapPin />, 'code': <Code />};
     
     return (
         <div className='skill'>
