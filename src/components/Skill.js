62679d755e5adf2424e7eaed4746660ce26f0703
Shelley Bassett <m.bassett@y7mail.com>
2022-05-16 10:28:42 +1000

    Added more icons, conditional displays

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Monitor, Users, Zap, Search, Activity, Award } from "react-feather";
+import { Monitor, Users, Zap, Search, Activity, Award, GitHub, Database, Cpu } from "react-feather";
 
 export default function Skill(props) {
-    const iconSvg = {'monitor': <Monitor />, 'users': <Users />, 'zap': <Zap />, 'search': <Search />, 'activity': <Activity />, 'award': <Award />};
+    const iconSvg = {'monitor': <Monitor />, 'users': <Users />, 'zap': <Zap />, 'search': <Search />, 'activity': <Activity />, 'award': <Award />, 'github': <GitHub />, 'database': <Database />, 'cpu': <Cpu />};
     
     return (
         <div className='skill'>
-            <div>{iconSvg[props.icon]}</div>
-            <h3>{props.heading}</h3>
-            <p>{props.desc}</p>
+            <div>{iconSvg[props.icon]? iconSvg[props.icon] : <Zap />}</div>
+            <h3>{props.heading ? props.heading : 'Detail Title'}</h3>
+            <p>{props.desc ? props.desc : 'Some details are coming soon'}</p>
         </div>
     )
 }
