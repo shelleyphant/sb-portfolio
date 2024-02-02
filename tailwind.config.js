9dc70695d1c7dd4056ff430c7d8713f829b67a84
Shelley Bassett <shelley@swim.com.au>
2024-02-03 08:54:14 +1100

    Added tailwind and defaults

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/pages/**/*.{js,jsx,ts,tsx}',
+        './src/components/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        colors: {
+            blue: '#a7c9de',
+            green: '#62b1a2',
+            'light-pink': '#fac6c2',
+            'dark-pink': '#ba436b',
+            'light-grey': '#f8f8f8',
+            'dark-grey': '#333333',
+            white: '#ffffff',
+            black: '#000000',
+        },
+        extend: {},
+    },
+    plugins: ['gatsby-plugin-postcss'],
+}
