eca1b0281d6e2b3549d111fab49ce7cee41dc84b
Shelley Bassett <shelley@swim.com.au>
2024-02-03 09:00:07 +1100

    Converted to Tailwind classes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,10 @@ import Footer from './Footer'
 
 export default function Layout({ children }) {
     return (
-        <div className="layout">
+        <div className="bg-light-grey p-12 min-h-screen">
             <Header />
 
-            <div className="wrapper">
+            <div className="bg-white container m-auto mt-14 rounded-md flex flex-col justify-between">
                 {children}
                 <Footer />
             </div>
